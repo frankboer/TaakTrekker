9f6e24df55c1fa8e0df603ff6767055364cc6c88
Frank <boer@bgenius.com>
2025-08-08 23:59:27 +0200

    Refactor job processing system: introduce configurator, replace JobQueue with JobRepository, and enhance job management with batching and status updates

--- taaktrekker-core/src/main/java/dev/frankboer/service/Poller.java
+++ taaktrekker-core/src/main/java/dev/frankboer/service/Poller.java
@@ -1,64 +1,71 @@
 package dev.frankboer.service;
 
 import dev.frankboer.domain.Job;
+import dev.frankboer.domain.JobStatus;
+import java.time.Duration;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Poller {
-    private final JobQueue jobQueue;
+    private final JobRepository jobRepository;
     private final JobWorker jobWorker;
     private final Listener listener;
-    private final ScheduledExecutorService scheduler;
-    private final ExecutorService jobExecutor;
+    private final Duration interval;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private final AtomicInteger slots = new AtomicInteger(0);
     private final Integer maxSlots;
     private volatile boolean running;
 
-    public Poller(JobQueue jobQueue, JobWorker jobWorker, Listener listener, int maxParallelJobs) {
-        this.jobQueue = jobQueue;
+    public Poller(
+            JobRepository jobRepository, JobWorker jobWorker, Listener listener, Duration interval, int parallelJobs) {
+        this.jobRepository = jobRepository;
         this.jobWorker = jobWorker;
         this.listener = listener;
-        this.scheduler = Executors.newScheduledThreadPool(1);
-        this.jobExecutor = Executors.newFixedThreadPool(maxParallelJobs);
-        this.maxSlots = maxParallelJobs;
+        this.interval = interval;
+        this.maxSlots = parallelJobs;
     }
 
     public void start() {
         running = true;
-        scheduler.scheduleWithFixedDelay(this::pollJobQueue, 0, 100, TimeUnit.MILLISECONDS);
+        scheduler.scheduleWithFixedDelay(this::pollJobQueue, 0, interval.toMillis(), TimeUnit.MILLISECONDS);
     }
 
     public void stop() {
         running = false;
         scheduler.shutdown();
-        jobExecutor.shutdown();
     }
 
     private void pollJobQueue() {
         if (!running) return;
 
         if (slots.get() < maxSlots) {
-            jobQueue.dequeue(maxSlots - slots.get())
-                    .forEach(job -> jobExecutor.submit(() -> {
-                        listener.onJobStarted(job);
-                        slots.incrementAndGet();
-                        try {
-                            jobWorker.run(job).whenComplete((result, error) -> {
-                                if (error != null) {
-                                    listener.onJobFailed(job);
-                                    jobQueue.updateStatus(job, Job.Status.FAILED);
-                                } else {
-                                    listener.onJobFinished(job);
-                                    jobQueue.updateStatus(job, Job.Status.COMPLETED);
-                                }
-                                slots.decrementAndGet();
-                            });
-                        } catch (Exception e) {
-                            listener.onJobFailed(job);
-                            jobQueue.updateStatus(job, Job.Status.FAILED);
-                            slots.decrementAndGet();
-                        }
-                    }));
+            jobRepository.dequeue(maxSlots - slots.get()).forEach(job -> {
+                listener.onJobStarted(job);
+                slots.incrementAndGet();
+                try {
+                    jobWorker.run(job).whenComplete((result, error) -> handleResult(job, error));
+                } catch (Exception e) {
+                    listener.onJobFailed(job);
+                    jobRepository.updateStatus(job, JobStatus.FAILED);
+                    slots.decrementAndGet();
+                }
+            });
+        }
+    }
+
+    private void handleResult(Job job, Throwable error) {
+        try {
+            slots.decrementAndGet();
+            if (error != null) {
+                jobRepository.updateStatus(job, JobStatus.FAILED);
+                listener.onJobFailed(job);
+            } else {
+                jobRepository.updateStatus(job, JobStatus.COMPLETED);
+                listener.onJobFinished(job);
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong:" + e);
+            slots.decrementAndGet();
         }
     }
 }
